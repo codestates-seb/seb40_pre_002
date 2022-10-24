bfc3eae04d8cada232ca157165cf9f3ca32a8c45
Seung-IL-Bang <seungilbang@khu.ac.kr>
2022-10-24 17:36:25 +0900

    BE: [Feat] added spring server (#3) (#4)
    * FE: Merge (#2)
    
    * added react
    
    * deleted everything
    
    * FE: [Feat] create react app
    
    Feat
    created app using cra -typescript
    
    * Delete Client directory
    
    * BE: [ADD] server
    
    * dir name
    
    Co-authored-by: akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
    
    Co-authored-by: akdlsz21 <81629070+akdlsz21@users.noreply.github.com>

---
+++ server/src/main/java/pre_002/stackOverFlow_Clone/StackOverFlowCloneApplication.java
@@ -0,0 +1,13 @@
+package pre_002.stackOverFlow_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowCloneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/pre_002/stackOverFlow_Clone/StackOverFlowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pre_002.stackOverFlow_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
