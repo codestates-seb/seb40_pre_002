c0c22ff74c0530814bc9623381e0041bcd6c3b5e
podoco <103816228+podoco@users.noreply.github.com>
2022-10-25 17:09:10 +0900

    axios

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -7,5 +7,4 @@ const instance = axios.create({
   },
   withCredentials: true,
 });
-
 export default instance;
--- client/src/api/requests.ts
+++ client/src/api/requests.ts
@@ -1,6 +1,5 @@
 const requests = {
   fetchUser: 'user/../..',
-  fetchPost: '/post/example',
 };
 
 export default requests;
