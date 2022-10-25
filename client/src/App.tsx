5bf1698143193fd0b1e95f73f138d3fbde112728
wongee93 <jhwon9304@gmail.com>
2022-10-25 11:53:26 +0900

    setting reactrouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      TEST
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <div className="App">
+          TEST
+        </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
