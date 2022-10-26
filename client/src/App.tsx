588806fc599bbc20c5f7d873e7ebd2b6696b8fd8
wongee93 <jhwon9304@gmail.com>
2022-10-26 11:01:37 +0900

    redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import styled from 'styled-components';
-import { decrement, increment } from './counterSlice'
-
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-      TEST
-=======
-      ssasTEST
-      <div>hellp</div>
->>>>>>> 7e350d849025d75baf3b281522b487eb73b83262
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { store } from './store'
+import App from './ReduxExample';
+import { store } from './redux/store/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
