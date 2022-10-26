37bcf5725f904c053b863a6ccb5f2c9f3907d3bc
podoco <103816228+podoco@users.noreply.github.com>
2022-10-26 16:53:19 +0900

    router setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Askpage from './pages/Askpage';
+import Detail from './pages/Detail';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="askpage" element={<Askpage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './ReduxExample';
-import { store } from './redux/store/store'
-import { Provider } from 'react-redux'
+import App from './App';
+import './index.css';
+import { store } from './redux/store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
