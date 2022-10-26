529ede81ea79e3e19e14f8117088179613a2b0e2
Seungil <seungilbang@khu.ac.kr>
2022-10-26 12:59:38 +0900

    [UPD] answer directory

--- server/src/main/java/pre_002/stackOverFlow_Clone/answer/controller/AnswerController.java
+++ server/src/main/java/pre_002/stackOverFlow_Clone/answer/controller/AnswerController.java
@@ -1,9 +1,28 @@
 package pre_002.stackOverFlow_Clone.answer.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import pre_002.stackOverFlow_Clone.answer.entity.Answer;
+import pre_002.stackOverFlow_Clone.answer.service.AnswerService;
 
 @Controller
+@RequiredArgsConstructor
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    @PostMapping("/questionList/{question-id}")
+    public ResponseEntity postAnswer(@PathVariable("question-id") Long questionId) {
+
+
+
+        return new ResponseEntity(new Answer(), HttpStatus.CREATED);
+    }
 
 
 }
