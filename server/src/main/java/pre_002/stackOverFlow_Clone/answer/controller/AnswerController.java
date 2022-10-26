8686be36d4b97a9bb2e9e5d771c4d088399a1528
Seungil <seungilbang@khu.ac.kr>
2022-10-27 00:08:43 +0900

    [FIX] Controller

--- server/src/main/java/pre_002/stackOverFlow_Clone/answer/controller/AnswerController.java
+++ server/src/main/java/pre_002/stackOverFlow_Clone/answer/controller/AnswerController.java
@@ -7,18 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pre_002.stackOverFlow_Clone.answer.entity.Answer;
 import pre_002.stackOverFlow_Clone.answer.service.AnswerService;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class AnswerController {
 
     private final AnswerService answerService;
 
-    @GetMapping("/")
-    public String test() {
-        return "hello ec2";
+    @GetMapping("/{test}")
+    public ResponseEntity test(@PathVariable("test") String test) {
+        Answer answer = new Answer();
+        answer.setContents(test);
+        Answer answer1 = answerService.create(answer);
+
+        return new ResponseEntity<>(test, HttpStatus.CREATED);
     }
 
     @PostMapping("/questionlist/{question-id}")
