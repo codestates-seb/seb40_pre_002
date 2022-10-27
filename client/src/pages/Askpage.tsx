41aded8abc8ff32a0004175ad84f24e660ab409e
Jihyun Won <jhwon9304@gmail.com>
2022-10-27 14:17:04 +0900

    FE: [Draft] Homepage css (#35)
    * Draft_homepage screen
    
    * modified css components api data

--- client/src/pages/Askpage.tsx
+++ client/src/pages/Askpage.tsx
@@ -1,5 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Askpage() {
-  return <div>Askpage</div>;
+  return (
+    <AskBody>
+
+      <AsideLeft>
+
+      </AsideLeft>
+
+      <AskMain>
+        <Qstitle>Ask a public question</Qstitle>
+
+        <Guide></Guide>
+
+        <TitleInput></TitleInput>
+        <ProblemInput></ProblemInput>
+        <ExpectiongInput></ExpectiongInput>
+
+      </AskMain>
+
+      <AsideRight>
+
+      </AsideRight>
+
+    </AskBody>
+  );
 }
+
+const AskBody = styled.body`
+  background-color: #F8F9F9;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  display: grid;
+  grid-template-columns: 100px auto 500px;
+`
+
+const AsideLeft = styled.aside`
+  
+`
+
+const AsideRight = styled.aside`
+  
+`
+
+const AskMain = styled.main`
+  padding: 10px;
+`
+
+const Guide = styled.div`
+  background-color: aliceblue;
+  border: solid 1px #0099ffea;
+  height: 250px;
+`
+
+const Qstitle = styled.div`
+  font-size: 25px;
+  font-weight: bold;
+  margin-top: 50px;
+  margin-bottom: 80px;
+`
+
+const TitleInput = styled.div`
+  height: 250px;
+  background-color: white;
+`
+
+const ProblemInput = styled.div`
+  height: 250px;
+  background-color: white;
+`
+
+const ExpectiongInput = styled.div`
+  height: 250px;
+  background-color: white;
+`
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,234 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+// Aside -> Component -> 외부로 빼기
+// 로그인 창에 nav바, aside가 안보여야 됨
+// Question info 컴포넌트화
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <HomeBody>
+
+      <AsideLeft>
+        <HomeBar>Home<Index /></HomeBar>
+      </AsideLeft>
+
+      <Main>
+        <MainHead>
+
+          <Question>
+            <Title>Top Questions</Title>
+            <AskLink to='askpage'>Ask Question</AskLink>
+          </Question>
+
+          <FilterBtnList>
+            <button><BountiedInfo>271</BountiedInfo> Bountied</button>
+            <button>Interesting</button>
+          </FilterBtnList>
+
+        </MainHead>
+
+        <ItemList>
+          <QuestionLi> 
+            <QuestionInfo1>
+              <VoteInfo>0 votes</VoteInfo>
+              <AnswerInfo>0 answers</AnswerInfo>
+              <ViewInfo>2 views</ViewInfo>
+            </QuestionInfo1>
+
+            <QuestionInfo2>
+              <TitleInfo>Title</TitleInfo>
+              <TagInfo><span>javascript</span></TagInfo>
+              <TimelineInfo><AuthorInfo>author</AuthorInfo><NumInfo>1</NumInfo>timeline info</TimelineInfo>
+            </QuestionInfo2>
+          </QuestionLi>
+        </ItemList>
+
+      </Main>
+
+      <AsideRight></AsideRight>
+    </HomeBody>
+  )
 }
+
+const HomeBody = styled.body`
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  display: flex;
+`
+
+const AsideLeft = styled.aside`
+  min-width: 245px;
+  height: 100vh;
+  border-right: solid 1px lightgray;
+`
+
+const AsideRight = styled.aside`
+  min-width: 450px;
+  height: 100vh;
+`
+const HomeBar = styled.div`
+  width: 143px;
+  height: 34px;
+  padding-left: 5px;
+  position: relative;
+  left: 98px;
+  margin-top: 20px;
+  background-color: #F1F2F3;
+  font-size: 15px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+`
+
+const Index = styled.span`
+  margin-left: 98px;
+  width: 5px;
+  height: 34px;
+  background-color: orange;
+`
+
+const Main = styled.main`
+  min-width: 800px;
+`
+
+const MainHead = styled.div`
+  height: 110px;
+  margin: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Title = styled.span`
+  font-size: 25px;
+`
+
+const Question = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const AskLink = styled(Link)`
+  width: 100px;
+  height: 37px;
+  background-color: #0099ffea;
+  box-shadow: 0.5px 0.5px 0.5px 0.5px #c2c4c7 inset;
+  color: white;
+  font-size: 13px;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`
+const FilterBtnList = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin-bottom: 15px;
+
+  >button{
+    height: 40px;
+    color: #686868;
+    background-color: white;
+    border: solid 1px #959595;
+    padding: 10px;
+  }
+`
+
+const BountiedInfo = styled.span`
+  background-color: #1D74cc;
+  color: white;
+  padding: 3px;
+  border-radius: 3px;
+`
+
+const ItemList = styled.ul`
+  padding: 0;
+`
+
+const QuestionLi = styled.li`
+  box-sizing: border-box;
+  padding-left: 0;
+  display: flex;
+  border-top: solid 1px #c2c4c7;
+  border-bottom: solid 1px #c2c4c7;
+`
+
+const QuestionInfo1 = styled.span`
+  width: 120px;
+  height: 110px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: right;
+`
+
+const VoteInfo = styled.div`
+    margin-top: 5px;
+    margin-bottom: 5px;
+    color: black;
+    font-weight: bold;
+    font-size: 13px;
+`
+
+const AnswerInfo = styled.div`
+    margin-top: 5px;
+    margin-bottom: 5px;
+    color: gray;
+    font-size: 13px;
+`
+
+const ViewInfo = styled.div`
+    margin-top: 5px;
+    margin-bottom: 5px;
+    color: gray;
+    font-size: 13px;
+`
+
+const QuestionInfo2 = styled.span`
+  margin-left: 20px;
+  width: 100%;
+  height: 110px;
+`
+const TitleInfo = styled.div`
+    margin-top: 25px;
+    margin-left: 25px;
+    font-size: 17px;
+    font-weight: bold;
+    color: #1D74cc;
+`
+
+const TagInfo = styled.div`
+    margin-top: 10px;
+    margin-left: 25px;
+
+    >span{
+      font-size: 15px;
+      padding: 5px;
+      color: #1D74cc;
+      background-color: #E1ECF4;
+    }
+`
+
+const TimelineInfo = styled.div`
+  position: relative;
+  right: -70%;
+  font-size: 14px;
+  color: gray;
+`
+
+const AuthorInfo = styled.span`
+  padding-right: 10px;
+  color: #1D74cc;
+`
+
+const NumInfo = styled.span`
+  color: #000000ca;
+  font-weight: bold;
+  margin-right: 10px;
+`
+
