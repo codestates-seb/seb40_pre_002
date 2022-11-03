d67aebf9b1887c604de0e73bf5b40077ca87667c
Seungil <seungilbang@khu.ac.kr>
2022-11-03 12:06:09 +0900

    [FIX] server_start.sh

--- server/scripts/server_start.sh
+++ server/scripts/server_start.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd /home/ubuntu/build
-
-ps -ef | grep "stackOverFlow_Clone-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}' | xargs kill -9 2> /dev/null
-
-if [ $? -eq 0 ];then
-	echo "application Stop Success"
-else
-	echo "application Not Running"
-fi
-
-echo "application start!"
-echo $1
-nohup java -jar stackOverFlow_Clone-0.0.1-SNAPSHOT.jar 1>stdout.txt 2>stderr.txt &
+sudo nohup java -jar stackOverFlow_Clone-0.0.1-SNAPSHOT.jar 1>stdout.txt 2>stderr.txt &
+sudo chmod 755 stdout.txt
