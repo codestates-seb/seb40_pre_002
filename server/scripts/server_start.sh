30a44fd5d23d74532d1a0436b818ab5811714bdd
Seungil <seungilbang@khu.ac.kr>
2022-11-03 11:58:18 +0900

    [FIX] refactoring application.yml & server_start.sh

--- server/scripts/server_start.sh
+++ server/scripts/server_start.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 cd /home/ubuntu/build
-sudo nohup java -jar stackOverFlow_Clone-0.0.1-SNAPSHOT.jar 1>stdout.txt 2>stderr.txt &
-sudo chmod 755 stdout.txt
+
+ps -ef | grep "stackOverFlow_Clone-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}' | xargs kill -9 2> /dev/null
+
+if [ $? -eq 0 ];then
+	echo "application Stop Success"
+else
+	echo "application Not Running"
+fi
+
+echo "application start!"
+echo $1
+nohup java -jar stackOverFlow_Clone-0.0.1-SNAPSHOT.jar 1>stdout.txt 2>stderr.txt &
